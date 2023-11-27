3aa5c948617153e8f6c61fa4f2767b6439118ca4
Ebani Gogia <42795015+ebanigogia@users.noreply.github.com>
2023-11-27 10:36:08 -0500

    Updated user class

--- Code/recommenderapp/app.py
+++ Code/recommenderapp/app.py
@@ -29,6 +29,21 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password_hash = db.Column(db.String(200))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 # Replace 'YOUR_API_KEY' with your actual OMDB API key
 OMDB_API_KEY = 'b726fa05'
 
