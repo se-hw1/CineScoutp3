b6e3da5d32554f1170194493ebced257d4955adc
Mihir Kamat <kamatmihir2002@gmail.com>
2024-10-30 14:52:55 -0400

    Improve coverage testing

--- .github/test.sh
+++ .github/test.sh
@@ -1,8 +1,12 @@
 cd src/backend
-python3 app.py &
+coverage run app.py &
 PID=`echo $!`
 cd ../..;
 sleep 5
 pytest --cov=.
 kill -9 $PID
+mv .coverage .coverage_a
+mv src/backend/.coverage .coverage_api
+coverage combine .coverage_a .coverage_api
+coverage report -m
 rm -rvf src/backend/instance
