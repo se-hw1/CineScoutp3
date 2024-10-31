2f121e88e30f739ed139ea504b594ee8b25b3c5a
Raj Kalantri <rkalant@ncsu.edu>
2024-10-30 22:29:57 -0400

    Added test cases for Top level app page

--- src/frontend/src/App.test.js
+++ src/frontend/src/App.test.js
@@ -1,8 +1,9 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './App'; // Adjust the path if needed
 
-test('renders learn react link', () => {
+test('renders welcome message', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const welcomeElement = screen.getByText(/welcome to cinescout/i);
+  expect(welcomeElement).toBeInTheDocument();
 });
